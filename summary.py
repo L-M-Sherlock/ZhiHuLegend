28a81e8aff40a13cb864ade71a54017a9c0dc3de
Jarrett Ye <jarrett.ye@outlook.com>
2024-11-25 11:40:36 +0800

    增加宇宙区长孙连城的知乎索引

--- summary.py
+++ summary.py
@@ -32,7 +32,8 @@ nicknames = {
     "wo-bu-zuo-lian-dao-hen-duo-nian": "我不做镰刀很多年",
     "an-guo-da-jiang-jun": "安国大将军",
     "shengjingjianke": "盛京剑客",
-    "pandamalone": "Long Yu"
+    "pandamalone": "Long Yu",
+    "gmqsunliancheng": "宇宙区长孙连城",
 }
 
 
